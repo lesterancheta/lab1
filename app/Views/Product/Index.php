d8ad83cae20d1c033f2eb3ed8f72118aa5dfa503
lesterancheta <lesterancheta322@gmail.com>
2023-09-22 00:05:21 +0800

    sheeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesh

--- app/Views/Product/Index.php
+++ app/Views/Product/Index.php
@@ -3,26 +3,74 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cool Product Listing</title>
 
     <!-- Add Bootstrap CSS Link -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            background-color: #333333;
+        }
+
+        .container-fluid {
+            min-height: 100vh;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+
+        h1 {
+            font-size: 36px;
+            color: #ffffff;
+            text-transform: uppercase;
+            margin-bottom: 20px;
+        }
+
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+
+        .btn-primary:hover {
+            background-color: #0056b3;
+            border-color: #0056b3;
+        }
+
+        .list-group {
+            width: 100%;
+            max-width: 400px;
+            margin-top: 20px;
+        }
+
+        .list-group-item {
+            background-color: #ffffff;
+            border-color: #cccccc;
+            font-size: 18px;
+            margin-top: 10px;
+        }
+
+        .list-group-item a {
+            color: #333333;
+            text-decoration: none;
+        }
+
+        .list-group-item:hover {
+            background-color: #f8f9fa;
+        }
+    </style>
 </head>
 <body>
-<div class="container-fluid bg-secondary" style="min-height: 100vh; display: flex; justify-content: center; align-items: center;">
-    <div class="row">
-        <div class="col text-center">
-            <h1>Product Listing</h1>
-            <a href="/product/create" class="btn btn-primary">Add New Product</a>
-            <ul class="list-group">
-                <?php foreach ($products as $product): ?>
-                <li class="list-group-item">
-                    <a href="/product/edit/<?= $product['id'] ?>"><?= $product['ProductName'] ?></a>
-                </li>
-                <?php endforeach; ?>
-            </ul>
-        </div>
-    </div>
+<div class="container-fluid">
+    <h1 class="text-center">Cool LAPTOP Product Listing</h1>
+    <a href="/product/create" class="btn btn-primary">Add New Product</a>
+    <ul class="list-group">
+        <?php foreach ($products as $product): ?>
+        <li class="list-group-item">
+            <a href="/product/edit/<?= $product['id'] ?>"><?= $product['ProductName'] ?></a>
+        </li>
+        <?php endforeach; ?>
+    </ul>
 </div>
 
 <!-- Add Bootstrap JS and jQuery -->
